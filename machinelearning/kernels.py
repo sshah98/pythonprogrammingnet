c43dba0cdd49442a96e80c9c70349ba509f941d3
sshah98 <shahsuraj261@gmail.com>
2018-05-23 12:00:15 -0400

    working on understanding kernels

---
+++ machinelearning/kernels.py
@@ -0,0 +1,11 @@
+# https://pythonprogramming.net/kernels-with-svm-machine-learning-tutorial
+# Kernels Introduction
+
+# a kernel is a similarity function - takes two inputs and outputs the similarity
+# np.inner is the same thing as np.dot 
+# inner product - projection of x1 over x2
+
+
+
+
+
